a5099b6acc2172c904f433ad9eccbb696c510e93
jason welsh <jasonbwelsh@gmail.com>
2017-05-23 18:53:08 -0400

    Changed button and bg styles

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 
-function playSound(e) {
+const background = document.querySelector('.keys');	
+
+function playSound(e) {	
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audio) return; //stops function if not an audio key
@@ -7,11 +9,13 @@ function playSound(e) {
   //before key can be hit again
   audio.play();
   key.classList.add('playing');
+  background.classList.add('bg-playing');
 }
 
 function removeTransition(e) {
   if(e.propertyName !== 'transform') return; // skip if not a transform
   this.classList.remove('playing');
+  background.classList.remove('bg-playing');
 }
 
 const keys = document.querySelectorAll('.key');
