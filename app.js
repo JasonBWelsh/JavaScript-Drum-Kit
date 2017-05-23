2fee297de06c59829db248a554cbe8038ec6788d
jason welsh <jasonbwelsh@gmail.com>
2017-05-23 18:26:15 -0400

    Basic Kit Created
    Basic kit created

---
+++ app.js
@@ -0,0 +1,20 @@
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if(!audio) return; //stops function if not an audio key
+  audio.currentTime = 0; //so audio does not have to fully play 
+  //before key can be hit again
+  audio.play();
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return; // skip if not a transform
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
